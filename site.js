2cbbe806520fd5065556ff872565d3861aba4aaa
Will VanderWyden <vanderwyden@gmail.com>
2025-02-04 19:07:27 +0900

    lots of extras

--- site.js
+++ site.js
@@ -7,8 +7,12 @@ function homeList() {
     allSongs.classList.add("hide-songs");
     const allVideo = document.querySelector(".all-video");
     allVideo.classList.add("hide-video");
-    const showContact = document.querySelector(".contact-form")
-    showContact.classList.add("hide-contact")
+    const showContact = document.querySelector(".contact-form");
+    showContact.classList.add("hide-contact");
+    document.querySelector(".u1").classList.add("hide-songs");
+    document.querySelector(".u2").classList.add("hide-video");
+    document.querySelector(".u3").classList.add("hide-contact");
+    stopVideos();
 
     // menu.forEach((span, index) => {
     //     switch(index) {
@@ -32,13 +36,18 @@ const songs = document.querySelector("#songs");
 songs.addEventListener("click", songList);
 
 function songList() {
-    const menu = document.querySelectorAll(".main-menu a");
+    // const menu = document.querySelectorAll(".main-menu a");
     const allSongs = document.querySelector(".all-songs");
     allSongs.classList.remove("hide-songs");
+    const newIdea = document.querySelector(".u1").classList.remove("hide-songs");
+    document.querySelector(".u2").classList.add("hide-video");
+    document.querySelector(".u3").classList.add("hide-contact");
     const allVideo = document.querySelector(".all-video");
-    allVideo.classList.add("hide-video")
-    const showContact = document.querySelector(".contact-form")
-    showContact.classList.add("hide-contact")
+    allVideo.classList.add("hide-video");
+    const showContact = document.querySelector(".contact-form");
+    showContact.classList.add("hide-contact");
+    
+    stopVideos();   
     // menu.forEach((span, index) => {
     //     switch(index) {
     //         case 1:
@@ -65,24 +74,33 @@ video.addEventListener("click", videoList);
 function videoList() {
     const allVideo = document.querySelector(".all-video");
     allVideo.classList.remove("hide-video");
+    const newIdea = document.querySelector(".u2").classList.remove("hide-video");
+    document.querySelector(".u1").classList.add("hide-songs");
+    document.querySelector(".u3").classList.add("hide-contact");
     const allSongs = document.querySelector(".all-songs");
     allSongs.classList.add("hide-songs");
     const showContact = document.querySelector(".contact-form")
     showContact.classList.add("hide-contact")
+    stopVideos();
 };
 
 const contact = document.querySelector("#contact");
 contact.addEventListener("click", contactForm);
 
 function contactForm() {
-    const showContact = document.querySelector(".contact-form")
-    showContact.classList.remove("hide-contact")
-    const bg = document.querySelector(".bg")
-    bg.classList.remove("hide-contact")
+    const showContact = document.querySelector(".contact-form");
+    showContact.classList.remove("hide-contact");
+    const bg = document.querySelector(".bg");
+    bg.classList.remove("hide-contact");
+    const newIdea = document.querySelector(".u3").classList.remove("hide-contact");
+    document.querySelector(".u1").classList.add("hide-songs");
+    document.querySelector(".u2").classList.add("hide-video");
 
     const menu = document.querySelectorAll(".main-menu a");
     const allSongs = document.querySelector(".all-songs");
     allSongs.classList.add("hide-songs");
     const allVideo = document.querySelector(".all-video");
     allVideo.classList.add("hide-video");
+    stopVideos();
 };
+
